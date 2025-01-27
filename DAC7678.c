c25f041930de16f5f2954376ea6794b489beb237
KnapMartin <martin_knap@yahoo.com>
2025-01-27 15:04:56 +0100

    inital

---
+++ DAC7678.c
@@ -0,0 +1,120 @@
+/*
+ * DAC7678.c
+ *
+ *  Created on: Jan 24, 2025
+ *      Author: knap-linux
+ */
+
+#include "DAC7678.h"
+
+#include <stdio.h>
+#include <string.h>
+
+static const uint8_t sDeviceAddress = 0x48 << 1;
+static uint8_t sInit = 0;
+
+DAC7678_State DAC7678_init(DAC7678 *device, I2C_HandleTypeDef *hi2c)
+{
+	device->m_hi2c = hi2c;
+	sInit = 1;
+
+	return DAC7678_OK;
+}
+
+DAC7678_State DAC7678_set_value(DAC7678 *device, const DAC7678_Channel channel, const DAC7678_WriteOptions options, const uint16_t value)
+{
+	if (!sInit) return DAC7678_ERROR;
+	if (value > DAC7678_MAX_VALUE) return DAC7678_ERROR;
+
+	uint8_t data[3];
+	if (options == DAC7678_UPDATE_ON)
+	{
+		data[0] = (DAC7678_WRITE_UPDATE << 4) | channel;
+	}
+	else
+	{
+		data[0] = (DAC7678_WRITE_IN_REG << 4) | channel;
+	}
+	data[1] = (uint8_t)(value >> 4);
+	data[2] = (uint8_t)(value << 4);
+
+	if (HAL_I2C_Master_Transmit(device->m_hi2c, sDeviceAddress, data, 3, DAC7678_TIMEOUT) != HAL_OK)
+	{
+		return DAC7678_ERROR;
+	}
+
+	return DAC7678_OK;
+}
+
+DAC7678_State DAC7678_update(DAC7678 *device)
+{
+
+}
+
+DAC7678_State DAC7678_set_internal_reference_static(DAC7678 *device, const DAC7678_ReferenceStaticOptions options)
+{
+	if (!sInit) return DAC7678_ERROR;
+
+	uint8_t data[3];
+	data[0] = DAC7678_WRITE_REF_STATIC;
+	data[1] = 0x00;
+	if (options == DAC7678_REF_ON)
+	{
+		data[2] = 0x10;
+	}
+	else
+	{
+		data[2] = 0x00;
+	}
+
+	if (HAL_I2C_Master_Transmit(device->m_hi2c, sDeviceAddress, data, 3, DAC7678_TIMEOUT) != HAL_OK)
+	{
+		return DAC7678_ERROR;
+	}
+
+	return DAC7678_OK;
+}
+
+DAC7678_State DAC7678_set_internal_reference_flexi(DAC7678 *device, const DAC7678_ReferenceFlexiOptions options)
+{
+	if (!sInit) return DAC7678_ERROR;
+
+	uint8_t data[3];
+	data[0] = DAC7678_WRITE_REF_FLEX;
+	data[2] = 0x00;
+	switch (options)
+	{
+		case DAC7678_REF_SYNCH_DAC:
+		{
+			data[1] = 0b01000000;
+			break;
+		}
+		case DAC7678_REF_ALWAYS_ON:
+		{
+			data[1] = 0b01010000;
+			break;
+		}
+		case DAC7678_REF_ALWAYS_OFF:
+		{
+			data[1] = 0b01100000;
+			break;
+		}
+		case DAC7678_REF_AS_STATIC:
+		{
+			data[1] = 0b00000000;
+			break;
+		}
+		default:
+		{
+			return DAC7678_OK;
+		}
+	}
+
+	if (HAL_I2C_Master_Transmit(device->m_hi2c, sDeviceAddress, data, 3, DAC7678_TIMEOUT) != HAL_OK)
+	{
+		return DAC7678_ERROR;
+	}
+
+	return DAC7678_OK;
+}
+
---
+++ DAC7678.h
@@ -0,0 +1,97 @@
+/*
+ * DAC7678.h
+ *
+ *  Created on: Jan 24, 2025
+ *      Author: knap-linux
+ */
+
+#ifndef DAC7678_H_
+#define DAC7678_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+#define DAC7678_TIMEOUT 	100 // ms
+#define DAC7678_MAX_VALUE 	4095
+
+typedef enum
+{
+	DAC7678_OK,
+	DAC7678_ERROR
+} DAC7678_State;
+
+typedef struct
+{
+	I2C_HandleTypeDef *m_hi2c;
+	uint8_t m_address;
+} DAC7678;
+
+typedef enum
+{
+	DAC7678_CH_A 	= 0b0000,
+	DAC7678_CH_B 	= 0b0001,
+	DAC7678_CH_C 	= 0b0010,
+	DAC7678_CH_D 	= 0b0011,
+	DAC7678_CH_E 	= 0b0100,
+	DAC7678_CH_F 	= 0b0101,
+	DAC7678_CH_G 	= 0b0110,
+	DAC7678_CH_H 	= 0b0111,
+	DAC7678_CH_ALL  = 0b1111
+} DAC7678_Channel;
+
+typedef enum
+{
+	DAC7678_WRITE_IN_REG      = 0b0000,
+	DAC7678_READ_IN_REG       = 0b0000,
+	DAC7678_UPDATE_DAC_REG    = 0b0001,
+	DAC7678_READ_DAC_REG      = 0b0001,
+	DAC7678_WRITE_UPDATE      = 0b0011,
+	DAC7678_WRITE_UPDATE_ALL  = 0b0010,
+	DAC7678_WRITE_OFF         = 0b0100,
+	DAC7678_READ_OFF          = 0b0100,
+	DAC7678_WRITE_CLR_CODE    = 0b0101,
+	DAC7678_READ_CLR_CODE     = 0b0101,
+	DAC7678_WRITE_LDAC        = 0b0110,
+	DAC7678_READ_LDAC         = 0b0110,
+	DAC7678_RESET             = 0b0111,
+	DAC7678_WRITE_REF_STATIC  = 0b1000,
+	DAC7678_READ_REF_STATIC   = 0b1000,
+	DAC7678_WRITE_REF_FLEX    = 0b1001,
+	DAC7678_READ_REF_FLEX     = 0b1001
+} DAC7678_Command;
+
+typedef enum
+{
+	DAC7678_UPDATE_ON,
+	DAC7678_UPDATE_OFF
+} DAC7678_WriteOptions;
+
+typedef enum
+{
+	DAC7678_REF_ON,
+	DAC7678_REF_OFF
+} DAC7678_ReferenceStaticOptions;
+
+typedef enum
+{
+	DAC7678_REF_SYNCH_DAC,
+	DAC7678_REF_ALWAYS_ON,
+	DAC7678_REF_ALWAYS_OFF,
+	DAC7678_REF_AS_STATIC,
+} DAC7678_ReferenceFlexiOptions;
+
+DAC7678_State DAC7678_init(DAC7678 *device, I2C_HandleTypeDef *hi2c);
+
+DAC7678_State DAC7678_set_value(DAC7678 *device, const DAC7678_Channel channel, const DAC7678_WriteOptions options, const uint16_t value);
+DAC7678_State DAC7678_update(DAC7678 *device);
+DAC7678_State DAC7678_set_internal_reference_static(DAC7678 *device, const DAC7678_ReferenceStaticOptions options);
+DAC7678_State DAC7678_set_internal_reference_flexi(DAC7678 *device, const DAC7678_ReferenceFlexiOptions options);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DAC7678_H_ */
